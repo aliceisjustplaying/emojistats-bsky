f123473e896193254df9eb30a761f0d6daf09247
alice <aliceisjustplaying@gmail.com>
2024-10-12 16:51:41 +0100

    configure log_interval and cursor_update_interval

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -6,8 +6,9 @@ export const FIREHOSE_URL = process.env.FIREHOSE_URL ?? 'wss://jetstream.atproto
 export const MAX_EMOJIS = 3790; // Per Unicode 16.0
 export const MAX_TOP_LANGUAGES = 30;
 export const EMIT_INTERVAL = 1000;
-export const LOG_INTERVAL = 10 * 1000;
-export const CURSOR_UPDATE_INTERVAL = 10 * 1000;
+export const LOG_INTERVAL = process.env.LOG_INTERVAL ? parseInt(process.env.LOG_INTERVAL, 10) : 10 * 1000;
+export const CURSOR_UPDATE_INTERVAL =
+  process.env.CURSOR_UPDATE_INTERVAL ? parseInt(process.env.CURSOR_UPDATE_INTERVAL, 10) : 10 * 1000;
 export const REDIS_URL = process.env.REDIS_URL ?? 'redis://localhost:6379';
 export const PORT = process.env.PORT ?? '3100';
 export const METRICS_PORT = process.env.METRICS_PORT ?? '3101';
