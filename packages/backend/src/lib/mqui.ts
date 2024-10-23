e29999349238a73826a8d467b735b89134d908dd
alice <aliceisjustplaying@gmail.com>
2024-10-23 23:19:14 +0100

    rename, shutdown

--- packages/backend/src/lib/mqui.ts
+++ packages/backend/src/lib/mqui.ts
@@ -4,9 +4,10 @@ import { FastifyAdapter } from '@bull-board/fastify';
 import fastify from 'fastify';
 
 import { BULLMQ_UI_PORT } from '../config.js';
+import logger from './logger.js';
 import { postQueue } from './queue.js';
 
-const run = async () => {
+const startBullMQUI = async () => {
   const app = fastify();
   const serverAdapter = new FastifyAdapter();
 
@@ -21,7 +22,8 @@ const run = async () => {
   const port = Number(BULLMQ_UI_PORT);
   await app.listen({ host: '0.0.0.0', port });
 
-  console.log(`For the UI, open http://localhost:${port}/`);
+  logger.info(`BullMQ UI available at http://localhost:${port}/`);
+  return app;
 };
 
-export { run };
+export { startBullMQUI };
